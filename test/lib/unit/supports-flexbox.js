c05ac89c7fb96f83a1a0e09ee50cb3a25ac0c3b9
Lila Conlee <lila.conlee@gmail.com>
2016-10-05 15:32:03 -0500

    Add support for Internet Explorer 9 and 10

---
+++ test/lib/unit/supports-flexbox.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var supportsFlexbox = require('../../../src/lib/supports-flexbox');
+
+describe('supportsFlexbox', function () {
+  beforeEach(function () {
+    this.fakeDiv = {
+      style: {
+        cssText: '',
+        length: 0
+      }
+    };
+  });
+
+  it('returns true in PhantomJS', function () {
+    expect(supportsFlexbox()).to.equal(true);
+  });
+
+  it("returns false for browsers that don't support flexbox", function () {
+    this.sandbox.stub(document, 'createElement').returns(this.fakeDiv);
+
+    expect(supportsFlexbox()).to.equal(false);
+  });
+
+  it('returns true if the browser supports flexbox', function () {
+    this.sandbox.stub(document, 'createElement').returns(this.fakeDiv);
+
+    this.fakeDiv.style.length = 1;
+
+    expect(supportsFlexbox()).to.equal(true);
+  });
+});
