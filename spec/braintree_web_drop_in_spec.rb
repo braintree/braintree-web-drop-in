05fb6329875b7cf42848a2709cdea291db582705
blade <blade.barringer@getbraintree.com>
2017-03-14 10:07:13 -0500

    Provide ability to configure test app

--- spec/braintree_web_drop_in_spec.rb
+++ spec/braintree_web_drop_in_spec.rb
@@ -10,13 +10,33 @@ describe "Drop-in" do
   include DropIn
   include PayPal
 
-  before :each do
-    visit "http://#{HOSTNAME}:#{PORT}"
+  describe "setup" do
+    it "requires a selector" do
+      visit "http://#{HOSTNAME}:#{PORT}?mergeWithDefault=true&selector=null"
+
+      expect(find("#id")).to have_content("options.selector is required.")
+    end
+
+    it "requires authorization" do
+      visit "http://#{HOSTNAME}:#{PORT}?mergeWithDefault=true&authorization=null"
+
+      expect(find("#id")).to have_content("options.authorization is required.")
+    end
+
+    it "does not setup paypal when not configured" do
+      visit "http://#{HOSTNAME}:#{PORT}?mergeWithDefault=true&paypal=null"
+
+      expect(page).to_not have_content("PayPal")
+      expect(page).to have_content("Card Number")
+      expect(page).to have_content("Expiration Date")
+    end
   end
 
   describe "tokenizes" do
     it "a card" do
       browser_skip("safari", "Testing iframes in WebKit does not work")
+
+      visit "http://#{HOSTNAME}:#{PORT}"
 
       click_option("Card")
       hosted_field_send_input("number", "4111111111111111")
@@ -26,22 +46,24 @@ describe "Drop-in" do
       expect(find(".braintree-heading")).to have_content("Paying with")
 
       # Drop-in Details
-      expect(page).to have_content('Ending in ••11')
+      expect(page).to have_content("Ending in ••11")
 
       # Nonce Details
-      expect(page).to have_content('CreditCard')
-      expect(page).to have_content('ending in 11')
-      expect(page).to have_content('Visa')
+      expect(page).to have_content("CreditCard")
+      expect(page).to have_content("ending in 11")
+      expect(page).to have_content("Visa")
     end
 
     it "PayPal" do
+      visit "http://#{HOSTNAME}:#{PORT}"
+
       click_option("PayPal")
 
       open_popup_and_complete_login
 
       expect(find(".braintree-heading")).to have_content("Paying with")
 
-      expect(page).to have_content('bt_buyer_us@paypal.com')
+      expect(page).to have_content("bt_buyer_us@paypal.com")
     end
   end
 end
