3f55b61a3881fb70639515de3c9f5eb9346e5f37
intelliot and jeremiespoken <code@getbraintree.com>
2017-03-24 16:51:28 +0000

    Improve loading transition
    - Alphabetize CSS and arrange into groups

--- src/views/payment-methods-view.js
+++ src/views/payment-methods-view.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var BaseView = require('./base-view');
+var classlist = require('../lib/classlist');
 var PaymentMethodView = require('./payment-method-view');
 
 var PAYMENT_METHOD_TYPE_TO_TRANSLATION_STRING = {
@@ -32,6 +33,18 @@ PaymentMethodsView.prototype._initialize = function () {
   for (i = paymentMethods.length - 1; i >= 0; i--) {
     this._addPaymentMethod(paymentMethods[i]);
   }
+
+  this.showMethodsLabel();
+};
+
+PaymentMethodsView.prototype.hideMethodsLabel = function () {
+  classlist.add(this._headingLabel, 'braintree-heading--inactive');
+}
+
+PaymentMethodsView.prototype.showMethodsLabel = function () {
+  setTimeout(function () {
+    classlist.remove(this._headingLabel, 'braintree-heading--inactive');
+  }.bind(this), 1);
 };
 
 PaymentMethodsView.prototype._getPaymentMethodString = function () {
