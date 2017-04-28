65678dcd475496b8b59e14c90b16ebb1eef063c7
blade <blade.barringer@getbraintree.com>
2017-04-28 08:55:48 -0500

    Put tokenization tests at top of file

--- spec/braintree_web_drop_in_spec.rb
+++ spec/braintree_web_drop_in_spec.rb
@@ -10,6 +10,61 @@ describe "Drop-in" do
   include DropIn
   include PayPal
 
+  describe "tokenizes" do
+    it "a card" do
+      visit "http://#{HOSTNAME}:#{PORT}"
+
+      click_option("card")
+      hosted_field_send_input("number", "4111111111111111")
+      hosted_field_send_input("expirationDate", "1019")
+      hosted_field_send_input("cvv", "123")
+
+      submit_pay
+
+      expect(find(".braintree-heading")).to have_content("Paying with")
+
+      # Drop-in Details
+      expect(page).to have_content("Ending in ••11")
+
+      # Nonce Details
+      expect(page).to have_content("CreditCard")
+      expect(page).to have_content("ending in 11")
+      expect(page).to have_content("Visa")
+    end
+
+    it "PayPal", :paypal do
+      visit "http://#{HOSTNAME}:#{PORT}"
+
+      click_option("paypal")
+
+      open_popup_and_complete_login
+
+      submit_pay
+
+      expect(find(".braintree-heading")).to have_content("Paying with PayPal")
+
+      expect(page).to have_content("PayPalAccount")
+      expect(page).to have_content(ENV["PAYPAL_USERNAME"])
+    end
+
+    it "PayPal Credit", :paypal do
+      visit "http://#{HOSTNAME}:#{PORT}"
+
+      click_option("paypalCredit")
+
+      open_popup_and_complete_login do
+        expect(page).to have_content("PayPal Credit");
+      end
+
+      submit_pay
+
+      expect(find(".braintree-heading")).to have_content("Paying with PayPal")
+
+      expect(page).to have_content("PayPalAccount")
+      expect(page).to have_content(ENV["PAYPAL_USERNAME"])
+    end
+  end
+
   describe "setup" do
     it "requires a selector" do
       visit "http://#{HOSTNAME}:#{PORT}?selector=null"
@@ -120,61 +175,6 @@ describe "Drop-in" do
       find('.braintree-toggle').click
 
       expect(page).to have_button('Pay', disabled: false)
-    end
-  end
-
-  describe "tokenizes" do
-    it "a card" do
-      visit "http://#{HOSTNAME}:#{PORT}"
-
-      click_option("card")
-      hosted_field_send_input("number", "4111111111111111")
-      hosted_field_send_input("expirationDate", "1019")
-      hosted_field_send_input("cvv", "123")
-
-      submit_pay
-
-      expect(find(".braintree-heading")).to have_content("Paying with")
-
-      # Drop-in Details
-      expect(page).to have_content("Ending in ••11")
-
-      # Nonce Details
-      expect(page).to have_content("CreditCard")
-      expect(page).to have_content("ending in 11")
-      expect(page).to have_content("Visa")
-    end
-
-    it "PayPal", :paypal do
-      visit "http://#{HOSTNAME}:#{PORT}"
-
-      click_option("paypal")
-
-      open_popup_and_complete_login
-
-      submit_pay
-
-      expect(find(".braintree-heading")).to have_content("Paying with PayPal")
-
-      expect(page).to have_content("PayPalAccount")
-      expect(page).to have_content(ENV["PAYPAL_USERNAME"])
-    end
-
-    it "PayPal Credit", :paypal do
-      visit "http://#{HOSTNAME}:#{PORT}"
-
-      click_option("paypalCredit")
-
-      open_popup_and_complete_login do
-        expect(page).to have_content("PayPal Credit");
-      end
-
-      submit_pay
-
-      expect(find(".braintree-heading")).to have_content("Paying with PayPal")
-
-      expect(page).to have_content("PayPalAccount")
-      expect(page).to have_content(ENV["PAYPAL_USERNAME"])
     end
   end
 end
