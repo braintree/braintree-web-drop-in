7d7ea39d2d6c61e3d7c8f3a4837f9e4dd3d55c8d
lilaconlee <lila.conlee@gmail.com>
2017-05-12 22:00:47 +0000

    Add initial documentation

--- src/dropin.js
+++ src/dropin.js
@@ -19,6 +19,66 @@ var svgHTML = fs.readFileSync(__dirname + '/html/svgs.html', 'utf8');
 var DEFAULT_CHECKOUTJS_LOG_LEVEL = 'warn';
 var VERSION = process.env.npm_package_version;
 
+// TODO current PayPal version helper
+
+/**
+ * @typedef {object} Dropin~cardPaymentMethodPayload
+ * @property {string} nonce The payment method nonce.
+ * @property {object} details Additional account details.
+ * @property {string} details.cardType Type of card, ex: Visa, MasterCard.
+ * @property {string} details.lastTwo Last two digits of card number.
+ * @property {string} description A human-readable description.
+ * @property {string} type The payment method type, always `CreditCard` when the method requested is a card.
+ */
+
+// TODO update PayPal details to be a link to the version of paypal-checkout used in this version
+/**
+ * @typedef {object} Dropin~paypalPaymentMethodPayload
+ * @property {string} nonce The payment method nonce.
+ * @property {object} details Additional PayPal account details. See a full list of details in the [PayPal client reference](http://braintree.github.io/braintree-web/current/PayPalCheckout.html#~tokenizePayload).
+ * @property {string} type The payment method type, always `PayPalAccount` when the method requested is a PayPal account.
+ */
+
+// TODO event example
+
+/**
+ * @name Dropin#on
+ * @function
+ * @param {string} event The name of the event to which you are subscribing.
+ * @param {function} handler A callback to handle the event.
+ * @description Subscribes a handler function to a named event. `event` should be {@link HostedFields#event:paymentMethodRequestable|paymentMethodRequestable} or {@link HostedFields#event:noPaymentMethodRequestable|noPaymentMethodRequestable}.
+ * @example
+ * <caption>Use events to disable a submit button</caption>
+ * @returns {void}
+ */
+
+// TODO type example
+
+/**
+ * This event is emitted when the payment method available in Drop-in changes. This includes when the state of Drop-in transitions from having no payment method available to having a payment method available and when the payment method available changes. This event is not fired if there is no payment method available on initialization. To check if there is a payment method requestable on initialization, use {@link Dropin#isPaymentMethodRequestable|isPaymentMethodRequestable}.
+ * @event Dropin#paymentMethodRequestable
+ * @type {Dropin~paymentMethodRequestablePayload}
+ * @example
+ * <caption>Get the type of payment method available</caption>
+ */
+
+/**
+ * @typedef {object} Dropin~paymentMethodRequestablePayload
+ * @description The event payload sent from {@link Dropin#on|on} with the {@link Dropin#event:paymentMethodRequestable|paymentMethodRequestable} event.
+ * @property {string} type The type of payment method that is requestable. Either `CreditCard` or `PayPalAccount`.
+ */
+
+/**
+ * This event is emitted when there is no payment method available in Drop-in. This event is not fired if there is no payment method available on initialization. To check if there is a payment method requestable on initialization, use {@link Dropin#isPaymentMethodRequestable|isPaymentMethodRequestable}. No payload is available in the callback for this event.
+ * @event Dropin#noPaymentMethodRequestable
+ */
+
+/**
+ * @class
+ * @param {object} options For create options, see {@link module:braintree-web-drop-in|dropin.create}.
+ * @description <strong>Do not use this constructor directly. Use {@link module:braintree-web-drop-in|dropin.create} instead.</strong>
+ * @classdesc This class represents a Drop-in component, that will create a pre-made UI for accepting cards and PayPal on your page. Instances of this class have methods for requesting a payment method and subscribing to events. For more information, see the [Drop-in guide](https://developers.braintreepayments.com/guides/drop-in/javascript/v3) in the Braintree Developer Docs. To be used in conjunction with the [Braintree Server SDKs](https://developers.braintreepayments.com/start/hello-server/).
+ */
 function Dropin(options) {
   this._client = options.client;
   this._componentID = uuid();
@@ -169,6 +229,12 @@ Dropin.prototype._disableErroredPaymentMethods = function () {
   }.bind(this));
 };
 
+/**
+ * Requests a payment method object, that includes the payment method nonce used by by the [Braintree Server SDKs](https://developers.braintreepayments.com/start/hello-server/). The structure of this object varies by type: a {@link Dropin~cardPaymentMethodPayload|cardPaymentMethodPayload} is returned when the payment method is a card, a {@link Dropin~paypalPaymentMethodPayload|paypalPaymentMethodPayload} is returned when the payment method is a PayPal account. If a payment method is not available, an error will appear in the UI and and error will be returned in the callback.
+ * @public
+ * @param {callback} callback The first argument will be an error if no payment method is available and will otherwise be null. The second argument will be an object containing a payment method nonce; either a {@link Dropin~paypalPaymentMethodPayload|paypalPaymentMethodPayload} or a {@link Dropin~paypalPaymentMethodPayload|paypalPaymentMethodPayload}.
+ * @returns {void}
+ */
 Dropin.prototype.requestPaymentMethod = function (callback) {
   this._mainView.requestPaymentMethod(callback);
 };
@@ -227,6 +293,12 @@ Dropin.prototype._getVaultedPaymentMethods = function (callback) {
   }
 };
 
+/**
+ * Cleanly remove anything set up by {@link module:braintree-web-drop-in|dropin.create}. This maybe be useful in a single-page app.
+ * @public
+ * @param {callback} [callback] Called on completion, containing an error if one occurred. No data is returned if teardown completes successfully.
+ * @returns {void}
+ */
 Dropin.prototype.teardown = function (callback) {
   this._removeStylesheet();
 
@@ -239,6 +311,11 @@ Dropin.prototype.teardown = function (callback) {
   }
 };
 
+/**
+ * Returns a boolean indicating if a payment method is available through {@link Dropin#requestPaymentMethod|requestPaymentMethod}.
+ * @public
+ * @returns {Boolean} True if a payment method is available, otherwise false.
+ */
 Dropin.prototype.isPaymentMethodRequestable = function () {
   return this._model.isPaymentMethodRequestable();
 };
--- src/index.js
+++ src/index.js
@@ -13,14 +13,60 @@ var DropinError = require('./lib/dropin-error');
 
 var VERSION = process.env.npm_package_version;
 
+// TODO add example
+// TODO use PayPal version for client reference link
+// TODO remove duplicate information from README
+
 /**
  * @static
  * @function create
- * @param {object} options Creation options:
- * @param {string} options.authorization TODO: authorization
- * @param {function} callback TODO: the callback
+ * @description This function is the entry point for `braintree.dropin`. It is used for creating {@link Dropin} instances.
+ * @param {object} options Object containing all {@link Dropin} options:
+ * @param {string} options.authorization A [tokenization key](https://developers.braintreepayments.com/guides/authorization/tokenization-key/javascript/v3) or a [client token](https://developers.braintreepayments.com/guides/authorization/client-token). If authorization is a client token created with a [customer ID](https://developers.braintreepayments.com/guides/drop-in/javascript/v3#customer-id), Drop-in will render saved payment methods and vault any new payment methods added.
+ * @param {string} options.selector A selector for an empty element, such as a `<div>`, where Drop-in will be included on your page. E.g. `#dropin-container`.
+ * @param {string} [options.locale=`en_US`] Use this option to change the language, links, and terminology used throughout Drop-in. Supported locales include:
+ * `da_DK`,
+ * `de_DE`,
+ * `en_US`,
+ * `en_AU`,
+ * `en_GB`,
+ * `es_ES`,
+ * `fr_CA`,
+ * `fr_FR`,
+ * `id_ID`,
+ * `it_IT`,
+ * `ja_JP`,
+ * `ko_KR`,
+ * `nl_NL`,
+ * `no_NO`,
+ * `pl_PL`,
+ * `pt_BR`,
+ * `pt_PT`,
+ * `ru_RU`,
+ * `sv_SE`,
+ * `th_TH`,
+ * `zh_CN`,
+ * `zh_HK`,
+ * `zh_TW`.
+ * @param {array} [options.paymentOptionPriority] Use this option to indicate the order in which enabled payment options should appear when multiple payment options are enabled. By default, payment options will appear in this order: `['card', 'paypal', 'paypalCredit']`. Payment options omitted from this array will not be offered to the customer.
+ *
+ * @param {object} [options.paypal] The configuration options for PayPal. To include a PayPal option in your Drop-in integration, include the `paypal` parameter and [enable PayPal in the Braintree Control Panel](https://developers.braintreepayments.com/guides/paypal/testing-go-live/#go-live). To test in Sandbox, you will need to [link a PayPal sandbox test account to your Braintree sandbox account](https://developers.braintreepayments.com/guides/paypal/testing-go-live/#linked-paypal-testing).
+ *
+ * Some of the PayPal configuration options are listed here, but for a full list see the [PayPal Checkout client reference options](http://braintree.github.io/braintree-web/current/PayPalCheckout.html#createPayment).
+ * @param {string} [options.paypal.flow] Either `checkout` for a one-time [Checkout with PayPal](https://developers.braintreepayments.com/guides/paypal/checkout-with-paypal/javascript/v3) flow or `vault` for a [Vault flow](https://developers.braintreepayments.com/guides/paypal/vault/javascript/v3). Required when using PayPal.
+ * @param {string|number} [options.paypal.amount] The amount of the transaction. Required when using the Checkout flow.
+ * @param {string} [options.paypal.currency] The currency code of the amount, such as `USD`. Required when using the Checkout flow.
+ *
+ * @param {object} [options.paypalCredit] The configuration options for PayPal Credit. To include a PayPal Credit option in your Drop-in integration, include the `paypalCredit` parameter and [enable PayPal in the Braintree Control Panel](https://developers.braintreepayments.com/guides/paypal/testing-go-live/#go-live).
+ *
+ * Some of the PayPal Credit configuration options are listed here, but for a full list see the [PayPal Checkout client reference options](http://braintree.github.io/braintree-web/current/PayPalCheckout.html#createPayment). For more information on PayPal Credit, see the [Braintree Developer Docs](https://developers.braintreepayments.com/guides/paypal/paypal-credit/javascript/v3).
+ * @param {string} [options.paypalCredit.flow] Either `checkout` for a one-time [Checkout with PayPal](https://developers.braintreepayments.com/guides/paypal/checkout-with-paypal/javascript/v3) flow or `vault` for a [Vault flow](https://developers.braintreepayments.com/guides/paypal/vault/javascript/v3). Required when using PayPal.
+ * @param {string|number} [options.paypalCredit.amount] The amount of the transaction. Required when using the Checkout flow.
+ * @param {string} [options.paypalCredit.currency] The currency code of the amount, such as `USD`. Required when using the Checkout flow.
+ * @param {function} callback The second argument, `data`, is the {@link Dropin} instance.
  * @returns {void}
  */
+
 function create(options, callback) {
   if (typeof callback !== 'function') {
     throw new DropinError('create must include a callback function.');
