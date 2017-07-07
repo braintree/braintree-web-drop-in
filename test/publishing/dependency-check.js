6b7fc0194a52a0d065e12c41d29277b8a8efe782
quinnjn <code@getbraintree.com>
2017-07-07 18:05:23 +0000

    Use npm view to get all nested dependencies

--- test/publishing/dependency-check.js
+++ test/publishing/dependency-check.js
@@ -1,28 +1,39 @@
 'use strict';
+var execSync = require('child_process').execSync;
 
 var dependencies = require('../../package.json').dependencies;
 var dependencyNames = Object.keys(dependencies);
-var packages = dependencyNames.filter(function (d) {
-  // Remove scoped packages
-  return d[0] !== '@';
+var dependencyPackages = dependencyNames.map(function (depName) {
+  var depPackages = {};
+  var depVersion = dependencies[depName];
+  var cmd = 'npm view --json ' + depName + '@' + depVersion + ' dependencies';
+  var result = execSync(cmd).toString();
+
+  if (result) {
+    depPackages = JSON.parse(result);
+  }
+
+  return {
+    name: depName,
+    dependencies: depPackages
+  };
 });
 
 describe('Shared dependency', function () {
-  packages.forEach(function (pkgName) {
-    describe(pkgName, function () {
-      var pkgDependencies = require('../../node_modules/' + pkgName + '/package.json').dependencies;
-      var sharedDependencyNames = Object.keys(pkgDependencies).filter(function (d) {
+  dependencyPackages.forEach(function (depPkg) {
+    describe(depPkg.name, function () {
+      var sharedDependencyNames = Object.keys(depPkg.dependencies).filter(function (d) {
         return dependencyNames.indexOf(d) !== -1;
       });
 
       sharedDependencyNames.forEach(function (sharedDepName) {
-        var sharedDepVersion = pkgDependencies[sharedDepName];
+        var sharedDepVersion = depPkg.dependencies[sharedDepName];
 
         it('uses ' + sharedDepName + '@' + sharedDepVersion, function () {
           var dependencyVersion = dependencies[sharedDepName];
 
           if (sharedDepVersion !== dependencyVersion) {
-            throw new Error(pkgName + ' should be using ' + sharedDepName + '@' + dependencyVersion + ' but was ' + sharedDepVersion);
+            throw new Error(depPkgName + ' should be using ' + sharedDepName + '@' + dependencyVersion + ' but was ' + sharedDepVersion);
           }
         });
       });
