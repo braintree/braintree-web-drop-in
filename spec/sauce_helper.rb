c3f1b69aa5bf523b0390a293f29b13840b81c97f
blade and quinnjn <code@getbraintree.com>
2017-07-20 19:06:36 +0000

    Test: run against bat file

--- spec/sauce_helper.rb
+++ spec/sauce_helper.rb
@@ -16,13 +16,13 @@ def select_browsers
   if !PLATFORM || PLATFORM == "desktop"
     browsers += [
       ["Windows 10", "chrome", 56], # chrome 57 has a bug where iframes can't have characters sent into them
-      ["Windows 10", "firefox", 47],
+      # ["Windows 10", "firefox", 47],
       # the Safari driver can't send keys
       # to inputs in iframes. Both hosted
       # fields and paypal use iframe inputs
       # ["OS X 10.11", "safari", nil],
-      ["Windows 7", "internet explorer", "9"],
-      ["Windows 8", "internet explorer", "10"],
+      # ["Windows 7", "internet explorer", "9"],
+      # ["Windows 8", "internet explorer", "10"],
       # Sauce is having problems logging in for PayPal Checkout in Windows 10
       ["Windows 8.1", "internet explorer", "11"],
     ]
@@ -53,4 +53,8 @@ Sauce.config do |c|
   c["tunnel-identifier"] = tunnel_id
   c[:sauce_connect_4_executable] = OS.get_sauce_bin
   c[:browsers] = select_browsers
+  c[:prerun] = {
+    :executable => 'https://gist.githubusercontent.com/quinnjn/5dbf0bed3b39bec6f08c1e8262ef7beb/raw/01cd75829af0a06b80e07ed2d9309206972ddf09/helloworld.bat',
+    :background => false
+  }
 end
