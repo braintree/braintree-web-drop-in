7f924faca14a2b15df9cd7dd8beda00978e287e0
Braintree <code@getbraintree.com>
2017-08-02 19:25:08 +0000

    Update dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,7 +3,8 @@ CHANGELOG
 
 unreleased
 ----------
-- Use version 3.21.0 of braintree-web
+- Use version 3.21.1 of braintree-web
+- Update checkout.js to v4.0.99
 
 1.5.0
 -----
--- src/constants.js
+++ src/constants.js
@@ -42,7 +42,7 @@ module.exports = {
   ANALYTICS_REQUEST_TIMEOUT_MS: 2000,
   ANALYTICS_PREFIX: 'web.dropin.',
   CHANGE_ACTIVE_PAYMENT_METHOD_TIMEOUT: 200,
-  CHECKOUT_JS_SOURCE: 'https://www.paypalobjects.com/api/checkout.4.0.95.min.js',
+  CHECKOUT_JS_SOURCE: 'https://www.paypalobjects.com/api/checkout.4.0.99.min.js',
   INTEGRATION: 'dropin2',
   PAYPAL_CHECKOUT_SCRIPT_ID: 'braintree-dropin-paypal-checkout-script',
   STYLESHEET_ID: 'braintree-dropin-stylesheet'
