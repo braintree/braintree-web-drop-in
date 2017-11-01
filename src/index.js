d0bce06a73d25f907c89187565964ea753af4d72
blade and kevinz <code@getbraintree.com>
2017-11-01 19:01:26 +0000

    Update apple pay docs

--- src/index.js
+++ src/index.js
@@ -142,13 +142,13 @@ var VERSION = process.env.npm_package_version;
 
 /** @typedef {object} applePayCreateOptions The configuration options for Apple Pay.
  *
- * @param {string} [buttonStyle=`black`] Configures the Apple Pay button style. Valid values are `black`, `white`, `white-outline`. Default value is `black`.
+ * @param {string} [buttonStyle=black] Configures the Apple Pay button style. Valid values are `black`, `white`, `white-outline`.
  * @param {string} displayName The canonical name for your store. Use a non-localized name. This parameter should be a UTF-8 string that is a maximum of 128 characters. The system may display this name to the user.
  * @param {external:ApplePayPaymentRequest} paymentRequest The payment request details to apply on top of those from Braintree.
  */
 
 /**
- * @typedef {object} ApplePayPaymentRequest An Apple Pay Payment Request object.
+ * @typedef {object} ApplePayPaymentRequest An [Apple Pay Payment Request object](https://developer.apple.com/reference/applepayjs/1916082-applepay_js_data_types/paymentrequest).
  * @external ApplePayPaymentRequest
  * @see {@link https://developer.apple.com/reference/applepayjs/1916082-applepay_js_data_types/paymentrequest PaymentRequest}
  */
