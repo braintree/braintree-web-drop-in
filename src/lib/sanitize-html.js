280415b8b2a360a9597f075be2ac5e4f46fd12f3
Blade Barringer <blade@crookedneighbor.com>
2018-02-26 11:43:51 -0600

    Remove unused code

--- src/lib/sanitize-html.js
+++ src/lib/sanitize-html.js
@@ -8,8 +8,4 @@ module.exports = function (string) {
   return string
     .replace(/</g, '&lt;')
     .replace(/>/g, '&gt;');
-    // https://stackoverflow.com/a/1354491/2601552
-    // .replace(/[\u00A0-\u00FF\u2022-\u2135]/g, function(char) {
-    //    return '&#' + char.charCodeAt(0) + ';';
-    // });
 };
