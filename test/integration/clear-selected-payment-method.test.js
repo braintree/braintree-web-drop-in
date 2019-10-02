0e69d0c545235d13c63ff63e16e18cf320f9ccb3
Braintree <code@getbraintree.com>
2019-10-02 15:16:54 +0000

    Port clear selected payment method tests to browserstack

--- test/integration/clear-selected-payment-method.test.js
+++ test/integration/clear-selected-payment-method.test.js
@@ -1,37 +1,41 @@
 require('./helper');
 
 describe('Drop-in#clearSelectedPaymentMethod', function () {
-  it("clears a credit card when it is the active payment method", function () {
+  this.retries(3);
+
+  it('clears a credit card when it is the active payment method', function () {
     browser.start();
 
-    browser.hostedFieldSendInput("number", "4111111111111111")
-    browser.hostedFieldSendInput("expirationDate", "1019")
-    browser.hostedFieldSendInput("cvv", "123")
+    browser.hostedFieldSendInput('number', '4111111111111111');
+    browser.hostedFieldSendInput('expirationDate', '1019');
+    browser.hostedFieldSendInput('cvv', '123');
 
     browser.submitPay();
 
-    expect($(".braintree-method.braintree-method--active").isExisting()).to.equal(true);
+    expect($('.braintree-method.braintree-method--active').isExisting()).to.equal(true);
 
-    $("#clear-button").click();
+    $('#clear-button').click();
 
-    expect($(".braintree-method.braintree-method--active").isExisting()).to.equal(false);
+    expect($('.braintree-method.braintree-method--active').isExisting()).to.equal(false);
   });
 
-  it("clears paypal when it is the active payment method", function () {
+  it('clears paypal when it is the active payment method', function () {
+    this.timeout(120000);
+
     browser.start({
       paypal: 'default'
     });
 
-    browser.clickOption("paypal")
+    browser.clickOption('paypal');
 
     browser.openPayPalAndCompleteLogin();
 
     browser.submitPay();
 
-    expect($(".braintree-method.braintree-method--active").isExisting()).to.equal(true);
+    expect($('.braintree-method.braintree-method--active').isExisting()).to.equal(true);
 
-    $("#clear-button").click();
+    $('#clear-button').click();
 
-    expect($(".braintree-method.braintree-method--active").isExisting()).to.equal(false);
+    expect($('.braintree-method.braintree-method--active').isExisting()).to.equal(false);
   });
 });
