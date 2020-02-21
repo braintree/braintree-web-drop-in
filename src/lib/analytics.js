098303d802bbaaf42a39ac84eabb02de2f326d9b
Braintree <code@getbraintree.com>
2020-02-21 04:49:01 +0000

    configure analtyics lib to use promise

--- src/lib/analytics.js
+++ src/lib/analytics.js
@@ -2,38 +2,47 @@
 
 var atob = require('./polyfill').atob;
 var constants = require('../constants');
+var Promise = require('./promise');
 var braintreeClientVersion = require('braintree-web/client').VERSION;
 
 function _millisToSeconds(millis) {
   return Math.floor(millis / 1000);
 }
 
-function sendAnalyticsEvent(client, kind, callback) {
-  var configuration = client.getConfiguration();
-  var analyticsRequest = client._request;
+function sendAnalyticsEvent(clientPromise, kind) {
   var timestamp = _millisToSeconds(Date.now());
-  var url = configuration.gatewayConfiguration.analytics.url;
-  var data = {
-    analytics: [{
-      kind: constants.ANALYTICS_PREFIX + kind,
-      timestamp: timestamp
-    }],
-    _meta: configuration.analyticsMetadata,
-    braintreeLibraryVersion: braintreeClientVersion
-  };
 
-  if (configuration.authorizationType === 'TOKENIZATION_KEY') {
-    data.tokenizationKey = configuration.authorization;
-  } else {
-    data.authorizationFingerprint = JSON.parse(atob(configuration.authorization)).authorizationFingerprint;
-  }
+  return Promise.resolve().then(function () {
+    return clientPromise;
+  }).then(function (client) {
+    var configuration = client.getConfiguration();
+    var url = configuration.gatewayConfiguration.analytics.url;
+    var data = {
+      analytics: [{
+        kind: constants.ANALYTICS_PREFIX + kind,
+        timestamp: timestamp
+      }],
+      _meta: configuration.analyticsMetadata,
+      braintreeLibraryVersion: braintreeClientVersion
+    };
 
-  analyticsRequest({
-    url: url,
-    method: 'post',
-    data: data,
-    timeout: constants.ANALYTICS_REQUEST_TIMEOUT_MS
-  }, callback);
+    if (configuration.authorizationType === 'TOKENIZATION_KEY') {
+      data.tokenizationKey = configuration.authorization;
+    } else {
+      data.authorizationFingerprint = JSON.parse(atob(configuration.authorization)).authorizationFingerprint;
+    }
+
+    return new Promise(function (resolve) {
+      client._request({
+        url: url,
+        method: 'post',
+        data: data,
+        timeout: constants.ANALYTICS_REQUEST_TIMEOUT_MS
+      }, function () {
+        resolve();
+      });
+    });
+  });
 }
 
 module.exports = {
--- test/unit/lib/analytics.js
+++ test/unit/lib/analytics.js
@@ -4,48 +4,24 @@ const atob = require('../../../src/lib/polyfill').atob;
 const braintreeClientVersion = require('braintree-web/client').VERSION;
 const constants = require('../../../src/constants');
 const fake = require('../../helpers/fake');
+const {
+  yieldsAsync
+} = require('../../helpers/yields');
 
 describe('analytics.sendEvent', () => {
   let testContext;
 
   beforeEach(() => {
     testContext = {};
-  });
-
-  beforeEach(() => {
     testContext.client = fake.client();
+    testContext.client._request.mockImplementation(yieldsAsync());
   });
 
-  test('correctly sends an analytics event with a callback', () => {
+  test('correctly sends an analytics event', () => {
     let postArgs, currentTimestamp;
     const fakeConfiguration = fake.configuration();
 
-    function callback() {}
-
-    analytics.sendEvent(testContext.client, 'test.event.kind', callback);
-
-    expect(testContext.client._request).toBeCalled();
-    postArgs = testContext.client._request.mock.calls[0];
-
-    expect(postArgs[0].url).toBe(fakeConfiguration.gatewayConfiguration.analytics.url);
-    expect(postArgs[0].method).toBe('post');
-    expect(postArgs[0].data.analytics[0].kind).toBe('web.dropin.test.event.kind');
-    expect(postArgs[0].data._meta.sessionId).toBe(fakeConfiguration.analyticsMetadata.sessionId);
-    currentTimestamp = Date.now() / 1000;
-    expect(currentTimestamp - postArgs[0].data.analytics[0].timestamp).toBeLessThan(2);
-    expect(currentTimestamp - postArgs[0].data.analytics[0].timestamp).toBeGreaterThan(0);
-    expect(postArgs[1]).toBe(callback);
-    expect(postArgs[0].timeout).toBe(constants.ANALYTICS_REQUEST_TIMEOUT_MS);
-  });
-
-  test(
-    'correctly sends an analytics event with no callback (fire-and-forget)',
-    () => {
-      let postArgs, currentTimestamp;
-      const fakeConfiguration = fake.configuration();
-
-      analytics.sendEvent(testContext.client, 'test.event.kind');
-
+    return analytics.sendEvent(testContext.client, 'test.event.kind').then(() => {
       expect(testContext.client._request).toBeCalled();
       postArgs = testContext.client._request.mock.calls[0];
 
@@ -57,19 +33,38 @@ describe('analytics.sendEvent', () => {
       expect(currentTimestamp - postArgs[0].data.analytics[0].timestamp).toBeLessThan(2);
       expect(currentTimestamp - postArgs[0].data.analytics[0].timestamp).toBeGreaterThan(0);
       expect(postArgs[0].timeout).toBe(constants.ANALYTICS_REQUEST_TIMEOUT_MS);
-    }
-  );
+    });
+  });
+
+  test('can receive a promise that resolves with a client', () => {
+    let postArgs, currentTimestamp;
+    const fakeConfiguration = fake.configuration();
+
+    return analytics.sendEvent(Promise.resolve(testContext.client), 'test.event.kind').then(() => {
+      expect(testContext.client._request).toBeCalled();
+      postArgs = testContext.client._request.mock.calls[0];
+
+      expect(postArgs[0].url).toBe(fakeConfiguration.gatewayConfiguration.analytics.url);
+      expect(postArgs[0].method).toBe('post');
+      expect(postArgs[0].data.analytics[0].kind).toBe('web.dropin.test.event.kind');
+      expect(postArgs[0].data._meta.sessionId).toBe(fakeConfiguration.analyticsMetadata.sessionId);
+      currentTimestamp = Date.now() / 1000;
+      expect(currentTimestamp - postArgs[0].data.analytics[0].timestamp).toBeLessThan(2);
+      expect(currentTimestamp - postArgs[0].data.analytics[0].timestamp).toBeGreaterThan(0);
+      expect(postArgs[0].timeout).toBe(constants.ANALYTICS_REQUEST_TIMEOUT_MS);
+    });
+  });
 
   test('correctly formats _meta', () => {
     let postArgs;
     const fakeConfiguration = fake.configuration();
 
-    analytics.sendEvent(testContext.client, 'test.event.kind');
+    return analytics.sendEvent(testContext.client, 'test.event.kind').then(() => {
+      expect(testContext.client._request).toBeCalled();
+      postArgs = testContext.client._request.mock.calls[0];
 
-    expect(testContext.client._request).toBeCalled();
-    postArgs = testContext.client._request.mock.calls[0];
-
-    expect(postArgs[0].data._meta).toEqual(fakeConfiguration.analyticsMetadata);
+      expect(postArgs[0].data._meta).toEqual(fakeConfiguration.analyticsMetadata);
+    });
   });
 
   test('includes tokenizationKey', () => {
@@ -78,14 +73,15 @@ describe('analytics.sendEvent', () => {
 
     fakeConfiguration.authorization = fake.tokenizationKey;
     client = fake.client(fakeConfiguration);
+    client._request.mockImplementation(yieldsAsync());
 
-    analytics.sendEvent(client, 'test.event.kind');
+    return analytics.sendEvent(client, 'test.event.kind').then(() => {
+      expect(client._request).toBeCalled();
+      postArgs = client._request.mock.calls[0];
 
-    expect(client._request).toBeCalled();
-    postArgs = client._request.mock.calls[0];
-
-    expect(postArgs[0].data.tokenizationKey).toBe(fakeConfiguration.authorization);
-    expect(postArgs[0].data.authorizationFingerprint).toBeFalsy();
+      expect(postArgs[0].data.tokenizationKey).toBe(fakeConfiguration.authorization);
+      expect(postArgs[0].data.authorizationFingerprint).toBeFalsy();
+    });
   });
 
   test('includes authorizationFingerprint', () => {
@@ -96,24 +92,25 @@ describe('analytics.sendEvent', () => {
     fakeConfiguration.authorizationType = 'CLIENT_TOKEN';
     fingerprint = JSON.parse(atob(fakeConfiguration.authorization)).authorizationFingerprint;
     client = fake.client(fakeConfiguration);
+    client._request.mockImplementation(yieldsAsync());
 
-    analytics.sendEvent(client, 'test.event.kind');
+    return analytics.sendEvent(client, 'test.event.kind').then(() => {
+      expect(client._request).toBeCalled();
+      postArgs = client._request.mock.calls[0];
 
-    expect(client._request).toBeCalled();
-    postArgs = client._request.mock.calls[0];
-
-    expect(postArgs[0].data.tokenizationKey).toBeFalsy();
-    expect(postArgs[0].data.authorizationFingerprint).toBe(fingerprint);
+      expect(postArgs[0].data.tokenizationKey).toBeFalsy();
+      expect(postArgs[0].data.authorizationFingerprint).toBe(fingerprint);
+    });
   });
 
   test('includes braintreeLibraryVersion', () => {
     let postArgs;
 
-    analytics.sendEvent(testContext.client, 'test.event.kind');
+    return analytics.sendEvent(testContext.client, 'test.event.kind').then(() => {
+      expect(testContext.client._request).toBeCalled();
+      postArgs = testContext.client._request.mock.calls[0];
 
-    expect(testContext.client._request).toBeCalled();
-    postArgs = testContext.client._request.mock.calls[0];
-
-    expect(postArgs[0].data.braintreeLibraryVersion).toBe(braintreeClientVersion);
+      expect(postArgs[0].data.braintreeLibraryVersion).toBe(braintreeClientVersion);
+    });
   });
 });
